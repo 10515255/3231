e508af0201699231d9355f392cf2cb8088f9e1fc
10515255 <10515255@student.uwa.edu.au>
2012-10-24 22:25:55 +0800

    Added reverse functions for converting hex to bytes

--- User/database.c
+++ User/database.c
@@ -10,7 +10,15 @@ unsigned char nibbleToHex(unsigned char nibble) {
 	}
 }
 
-char *convertToHex(unsigned char *bytes, int length) {
+unsigned char hexToNibble(unsigned char hex) {
+	if('0' <= hex && hex <= '9') return hex - '0';
+	if('A' <= hex && hex <= 'F') return hex - 'A' + 10;
+	else {
+		fprintf(stderr, "Unexpected value passed to hexToNibble()\n");
+		return '0';
+	}
+}
+unsigned char *bytesToHex(unsigned char *bytes, int length) {
 	unsigned char *hex = malloc(length*2);
 	if(hex == NULL) {
 		fprintf(stderr, "malloc() failed in convertToHex()\n");
@@ -28,11 +36,42 @@ char *convertToHex(unsigned char *bytes, int length) {
 	return hex;
 }
 
+unsigned char *hexToBytes(unsigned char *hex, int length) {
+	if(length % 2 != 0) {
+		fprintf(stderr, "bytesFromHex() passed odd number of hex chars.\n");
+		return NULL;
+	}
+	int numBytes = length / 2;
+	unsigned char *bytes = malloc(numBytes);
+	if(bytes == NULL) {
+		fprintf(stderr, "malloc() failed in bytesFromHex()\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for(int i=0; i < numBytes; ++i) {
+		//byte = lower + upper << 4
+		unsigned char upper = hexToNibble(hex[2*i]);
+		unsigned char lower = hexToNibble(hex[2*i+1]);
+		bytes[i] = lower | (upper << 4); 
+	}
+
+	return bytes;
+}
+
 int main() {
-	unsigned char *c = "penguin";
-	char *hex = convertToHex(c, 7);
-	for(int i=0; i<14; ++i) {
+	unsigned char *c = "0F00FFA9";
+	char *hex = hexToBytes(c, 8);
+	for(int i=0; i<4; ++i) {
 		putchar(hex[i]);
 	}
+	putchar('\n');
+
+	char *bytes = bytesToHex(hex, 4);
+	for(int i=0; i<8; ++i) {
+		putchar(bytes[i]);
+	}
+
+	free(bytes);
+	free(hex);
 }
 

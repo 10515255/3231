58babe3c2e60a3ad03d831d1c4041d4e9ca47a20
10515255 <10515255@student.uwa.edu.au>
2012-10-25 10:08:30 +0800

    Worked on Bank database a bit.

--- Bank/database.c
+++ Bank/database.c
@@ -10,54 +10,71 @@
 
 #define MAX_LINE_LENGTH 64
 
-/* An array of pointers our users. */
-USER **users;
-int numUsers;
+/* Only used internally to this file at the moment */
+typedef struct {
+	int id;
+	int balance;
+} USER;
 
+/* The file which acts as a database for customers and their information. */
+char *usersFilename = "users.txt";
+/* The file which acts as a database for serial numbers of issued bank notes */
+char *serialsFilename = "serials.txt";
 
-int loadUsers(char *usersFile) {
-	FILE *ifp = fopen(usersFile, "r");
+char *tempFilename = "temp.tmp";
+
+/* Remove the line in the file which has the given number
+ * at the front of the line */
+int removeLine(char *filename, int targetNumber) {
+	FILE *ifp = fopen(filename, "r");
 	if(ifp == NULL) {
-		perror("loadUsers");
+		perror("removeLine");
 		return -1;
 	}
-	
-	numUsers = -1;
-	int matched = fscanf(ifp, "%d", &numUsers);
-	if(matched != 1) {
-		perror("loadUsers");
-		return -2;
-	}
-	if(numUsers < 0) {
-		return -3;
+
+	FILE *ofp = fopen(tempFilename, "w");
+	if(ofp == NULL) {
+		perror("removeLine");
+		return -1;
 	}
 
-	users = malloc(sizeof(USER *) * numUsers);
-	if(users == NULL) {
-		perror("loadUsers");
-		exit(EXIT_FAILURE);
-	}
-	for(int i=0; i<numUsers; ++i) {
-		users[i] = malloc(sizeof(USER));
-		if(users[i] == NULL) {
-			perror("loadUsers");
-			exit(EXIT_FAILURE);
+	char line[MAX_LINE_LENGTH];
+	bool foundLine = false;
+	while(fgets(line, sizeof(line), ifp) != NULL) {
+		int serial; 
+		int matched = sscanf(line, "%d", &serial);
+		if(matched != 1) {
+			perror("removeLine");
+			return -1;
+		}
+
+		//print out all lines except that which begins with the target number
+		if(serial != targetNumber) {
+			fprintf(ofp, "%s", line);
+		} 
+		else {
+			foundLine = true;
 		}
 	}
 
-	for(int i=0; i<numUsers; ++i) {
-		int matched = fscanf(ifp, "%d %d", &(users[i]->userid), &(users[i]->balance));
-		if(matched != 2) {
-			perror("loadUsers");
-			return -4;
-		}
+	//close all files and rename back to original filename
+	fclose(ifp);
+	fclose(ofp);
+	remove(filename);	
+	int status = rename(tempFilename, filename);
+	if(status < 0) {
+		perror("removeNumber");
+		return -1;
 	}
-	
-	return 0;
+
+	if(foundLine) return 0;
+	return -1;
 }
 
-int addSerial(char *serialFile, int serial) {
-	FILE *ofp = fopen(serialFile, "a"); 
+
+/* Add the given serial number to the serials file */
+int addSerial(int serial) {
+	FILE *ofp = fopen(serialsFilename, "a"); 
 	if(ofp == NULL) {
 		perror("addSerial");
 		return -1;
@@ -69,74 +86,76 @@ int addSerial(char *serialFile, int serial) {
 	return 0;
 }
 
-int removeSerial(char *serialFile, int targetSerial) {
-	FILE *ifp = fopen(serialFile, "r");
-	if(ifp == NULL) {
-		perror("removeSerial");
-		return -2;
-	}
-
-	FILE *ofp = fopen("temp.j", "w");
-	if(ofp == NULL) {
-		perror("removeSerial");
-		return -3;
-	}
-
-	char line[MAX_LINE_LENGTH];
-	bool foundSerial = false;
-	while(fgets(line, sizeof(line), ifp) != NULL) {
-		int serial; 
-		int matched = sscanf(line, "%d", &serial);
-		if(matched != 1) {
-			perror("removeSerial");
-			return -4;
-		}
-
-		if(serial != targetSerial) {
-			fprintf(ofp, "%s", line);
-		} 
-		else {
-			foundSerial = true;
-		}
-	}
-
-	fclose(ifp);
-	fclose(ofp);
-
-	remove(serialFile);	
-	int status = rename("temp.j", serialFile);
-	if(status < 0) {
-		perror("removeSerial");
-		return -5;
-	}
-
-	if(foundSerial) return 0;
-	
-	return -1;
+/* Remove the given serial number from the serials file */
+int removeSerial(int targetSerial) {
+	return removeLine(serialsFilename, targetSerial);
 }
 
-int queryBalance(int id) {
-	for(int i=0; i<numUsers; i++) {
-		if(users[i]->userid == id) {
-			return users[i]->balance;
+/* Read a users info from a line in the given file. */
+USER *readUser(FILE *userFile) {
+	//static user here, fill it and return pointer to it
+	static USER user;
+
+	char line[MAX_LINE_LENGTH];
+	if(fgets(line, sizeof(line), userFile) == NULL) return NULL;
+	
+	int numMatched = sscanf(line, "%d %d", &user.id, &user.balance);
+	if(numMatched != 2) {
+		fprintf(stderr, "sscanf failed in readUser() on the line:\n");
+		fprintf(stderr, "%s", line);
+		return NULL;
+	}
+	return &user;
+}
+
+/* Write a users info to the given file stream */
+void writeUser(FILE *userFile, USER *user) {
+	fprintf(userFile, "%d %d\n", user->id, user->balance);
+}
+
+/* Load the user with the given id into a statically allocated USER struct
+ * and return a pointer to it */
+USER *getUser(int targetID) {
+	//load the users file
+	FILE *usersFile = fopen(usersFilename, "r");
+	if(usersFile == NULL) {
+		perror("getBalance()");
+		return NULL;
+	}
+
+	//read the file until we find this users line
+	char line[MAX_LINE_LENGTH];
+	USER *user;
+	while( (user = readUser(usersFile)) != NULL ) {
+		if(user->id == targetID) {
+			fclose(usersFile);
+			return user;
 		}
 	}
 
-	fprintf(stderr, "queryBalance: No user with id %d\n", id);
+	//no user with the given id on file
+	fprintf(stderr, "getUser() called on non-existant user %u\n", targetID);
+	fclose(usersFile);
+	return NULL;
+}
+
+int removeUser(int targetID) {
+	return removeLine(usersFilename, targetID);
+}
+
+int getBalance(int id) {
+	USER *user = getUser(id);
+	if(user != NULL) return user->balance;
+	
 	return -1;
 }
 
 int main(int argc, char **argv)
 {
-	if(argc < 2) {
-		printf("Insufficient arguments.\n");
+	for(int i=0; i<5; ++i) {
+		int x = getBalance(i);
+		if(x != -1)	printf("User %d has %d dollars.\n", i, x);
+		else printf("No user %d\n", i);
 	}
-	loadUsers(argv[1]);
 
-	addSerial("serials.txt", 1);
-	addSerial("serials.txt", 2);
-	addSerial("serials.txt", 3);
-	addSerial("serials.txt", 4);
-
-	removeSerial("serials.txt", 2);
 }
--- Bank/database.h
+++ Bank/database.h
@@ -1,4 +1,13 @@
-typedef struct {
-	int userid;
-	int balance;
-} USER;
+/* Add the given serial number to our database of
+ * issued serial numbers */
+int addSerial(int serial);
+/* Remove the given serial number from our database
+ * of issued serial numbers */
+int removeSerial(int targetSerial);
+
+/* Get the balance of the user with the given id
+ * from our database of users. */
+int getBalance(int targetID);
+/* Remove the user with the given id from our 
+ * database of users. */
+int removeUser(int targetID);
